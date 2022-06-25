bdba1fb268cf17627ec3155dbc3b32093db0beb5
tpemba100 <tpemba.96@gmail.com>
2022-06-25 01:00:05 -0400

    wrapper container slider and buttons

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,39 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@mui/icons-material"
+import { useRef, useState } from "react"
 import ListItem from "../listItem/ListItem"
 import "./list.scss"
 
 export default function List() {
+  const [isMoved, setIsMoved] = useState(0);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === "left" && slideNumber > 0){
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+
+    if(direction === "right" && slideNumber < 5){
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`
+    }
+    console.log(distance)
+  }
+
   return (
     <div className="list">
         <span className="listTitle">Continue to watch</span>
         <div className="wrapper">
-            <ArrowBackIosOutlined/>
-            <div className="container">
+            <ArrowBackIosOutlined 
+              className="sliderArrow left" 
+              onClick={()=>handleClick("left")}
+              style={{display: !isMoved && "none"}}
+            />
+            <div className="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
@@ -20,7 +45,7 @@ export default function List() {
                 <ListItem/>
                 <ListItem/>
             </div>
-            <ArrowForwardIosOutlined/>
+            <ArrowForwardIosOutlined className="sliderArrow right" onClick={()=>handleClick("right")}/>
         </div>
     </div>
   )
