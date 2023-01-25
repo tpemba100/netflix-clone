d0b239b978107463c3fd51bb82d8bb703eba0add
tpemba100 <tpemba.96@gmail.com>
2023-01-25 14:53:55 -0500

    Rest API routes for List. Query based on type or genre

---
+++ api/routes/lists.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const List = require("../models/List");
+const verify = require("../verifyToken");
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newList = new List(req.body);
+    try {
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//DELETE
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("The list has been delete...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//GET
+
+router.get("/", verify, async (req, res) => {
+  const typeQuery = req.query.type; //movie or series query
+  const genreQuery = req.query.genre; //genres query
+  let list = [];
+
+  try {
+    if (typeQuery) {
+      // if there is query for type.
+      if (genreQuery) {
+        // if genre query -> aggregate 10 movie & matches query with type and genre
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, genre: genreQuery } },
+        ]);
+      } else {
+        // if no genre query -> only match query with type query
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
+    } else {
+      // if no type query, aggregate random samples of 10
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
+    }
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
