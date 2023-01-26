76bb00b21afd128bcf73400108a77034f9713965
tpemba100 <tpemba.96@gmail.com>
2023-01-26 15:08:59 -0500

    user is true

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.scss";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
-  const user = false;
+  const user = true;
   return (
     <div>
       {/* <Watch /> */}
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={user? <Home /> : <Register />}  />
-        <Route path="/movies" element={<Home type="movie" />}  />
+        <Route path="/home" element={user ? <Home /> : <Register />} />
+        <Route path="/movies" element={<Home type="movie" />} />
         <Route path="/series" element={<Home type="series" />} />
         <Route path="/watch" element={<Watch />} />
       </Routes>
