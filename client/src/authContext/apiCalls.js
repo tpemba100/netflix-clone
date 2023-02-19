2e9047c7f99633c33a2824b5526185a52d6b4ea3
tpemba100 <tpemba.96@gmail.com>
2023-02-19 15:28:22 -0500

    backend url

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -4,8 +4,11 @@ import { loginFailure, loginSuccess, loginStart } from "./AuthAction";
 export const doLogin = async (user, dispatch) => {
   dispatch(loginStart());
   console.log("data user 0");
+
+  const url = "https://netflix-clone-api-9ydn.onrender.com";
+
   try {
-    const res = await axios.post("/api/auth/login", user);
+    const res = await axios.post(url + `/api/auth/login`, user);
     dispatch(loginSuccess(res.data));
     console.log("data user 1");
     console.log(user);
